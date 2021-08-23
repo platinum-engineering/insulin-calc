6fb8a28adfdd6134b3e1e4748a40627f4cae3357
maxsiz <maxsizmobile@gmail.com>
2021-08-23 17:02:38 +0800

    #1 #2 #3 #4 #5   updates

--- tests/test_write_events.py
+++ tests/test_write_events.py
@@ -4,7 +4,8 @@ from brownie import *
 
 LOGGER = logging.getLogger(__name__)
 
-MEASURE_RECORD =  [58,50,6,('Novorapid',3)]
+#MEASURE_RECORD =  [58,50,6,('Novorapid',3)]
+MEASURE_RECORD =  [58,50,6,1]
 
 def test_write_event(accounts, storage1):
     tx = storage1.bolus(
@@ -19,24 +20,35 @@ def test_write_event(accounts, storage1):
     assert tx.events['MeasureRecord']['glucose'] == MEASURE_RECORD[0]
     assert tx.events['MeasureRecord']['carbs'] == MEASURE_RECORD[1]
     assert tx.events['MeasureRecord']['units'] == MEASURE_RECORD[2]
-    assert tx.events['MeasureRecord']['insulinType'] == MEASURE_RECORD[3]
+    #assert tx.events['MeasureRecord']['insulinType'] == MEASURE_RECORD[3]
 
     
 def test_get_insulins(accounts, storage1):
-    logging.info(storage1.getInsulins())
-    assert len(storage1.getInsulins()) == 3
+    logging.info(storage1.getInsulins(accounts[0]))
+    assert len(storage1.getInsulins(accounts[0])) == 2
 
 def test_save_personal_insulin(accounts, storage1):
-    storage1.addPersonalInsulin('personal_insulin 1', {'from': accounts[0]})
-    storage1.addPersonalInsulin('personal_insulin 2', {'from': accounts[0]})
-    storage1.addPersonalInsulin('personal_insulin 3', {'from': accounts[0]})
-    assert len(storage1.getPersonalInsulins(accounts[0]))==3
+    storage1.addInsulin('personal_insulin 1',100500, {'from': accounts[0]})
+    storage1.addInsulin('personal_insulin 2',200500, {'from': accounts[0]})
+    storage1.addInsulin('personal_insulin 3',300500, {'from': accounts[0]})
+    assert len(storage1.getInsulins(accounts[0]))==3
 
-def test_get_all_insulin(accounts, storage1):
-    logging.info(storage1.getAllInsulins())
-    assert len(storage1.getAllInsulins())==6
+# def test_get_all_insulin(accounts, storage1):
+#     logging.info(storage1.getInsulins())
+#     assert len(storage1.getAllInsulins())==6
 
 def test_remove_personal_insulin(accounts, storage1):
-    storage1.removePersonalInsulin(1, {'from': accounts[0]})
-    assert len(storage1.getPersonalInsulins(accounts[0]))==2
-    logging.info(storage1.getAllInsulins())     
+    storage1.removeInsulin(1, {'from': accounts[0]})
+    assert len(storage1.getInsulins(accounts[0]))==2
+    logging.info(storage1.getInsulins(accounts[0]))  
+
+def test_write_event_with_personal(accounts, storage1):
+    tx = storage1.bolus(
+            chain.time(),
+            MEASURE_RECORD[0],
+            MEASURE_RECORD[1],
+            MEASURE_RECORD[2],
+            MEASURE_RECORD[3]
+        )
+    logging.info(tx.events['MeasureRecord'])
+    assert tx.events['MeasureRecord']['insulinType'][0] == 'personal_insulin 3'        
